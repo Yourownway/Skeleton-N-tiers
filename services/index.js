f245c89ecae168a52f52f47e52f9b012f0fad4bf
Chuck <lorris.dhedin@hotmail.fr>
2021-02-24 10:47:55 +0100

    add gitignore

--- services/index.js
+++ services/index.js
@@ -1,16 +1,17 @@
 // service dépendencies
-const repositories = require('../repositories/index'),
-    nodemailer = require("nodemailer");
+const repositories = require("../repositories/index"),
+  nodemailer = require("nodemailer"),
+  bcrypt = require("bcrypt");
 
 // services
-const user_service = require('./user'),
-    mailer_service = require('./mailer');
+const user_service = require("./user"),
+  mailer_service = require("./mailer");
 
 // create a services object for map all the services
 const services = {
-    user: user_service(repositories),
-    mailer: mailer_service(nodemailer)
-}
+  user: user_service(repositories, bcrypt),
+  mailer: mailer_service(nodemailer),
+};
 
 // export our service object
 module.exports = services;
